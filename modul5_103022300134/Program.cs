4ec9813848a4d08042575ee65da826be0a108c78
Ade Fathia Nuraini <adfthn4@gmail.com>
2025-03-16 19:31:50 +0700

    menambahkan class DataGeneric

--- modul5_103022300134/Program.cs
+++ modul5_103022300134/Program.cs
@@ -1,5 +1,18 @@
 ﻿using modul5_103022300134;
 
-var halo = new HaloGeneric();
-halo.SapaUser("Aufa");
-halo.SapaUser(123);
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Membuat instance dari DataGeneric dengan tipe data string (gunakan NIM sebagai data)
+        DataGeneric<string> dataGeneric = new DataGeneric<string>("1030223000134");
+
+        // Memanggil method PrintData untuk mencetak data
+        dataGeneric.PrintData();
+
+        Console.ReadLine(); // Agar console tidak langsung tertutup
+    }
+}
+
+
+
